73381682cbc496356c9736f47619e38fb1b463f4
unknown <andrew603@gmail.com>
2025-03-18 20:56:07 -0700

    the completed orders

--- user-completed.php
+++ user-completed.php
@@ -187,5 +187,6 @@
     <script src="script/sidebarActive2.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="script/purchase-notification.js"></script>
+    <script src="script/completed-items.js"></script>
 </body>
 </html>
